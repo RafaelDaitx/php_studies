b893ac3b18d82095b59a02f8f7f328e6d231840f
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2025-03-01 21:26:09 -0300

    creating sql

--- public/index.php
+++ public/index.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once(dirname(__FILE__, 2). '/src/config/database.php');
+require_once(dirname(__FILE__, 2). '/src/config/config.php');
 //o 2 representa o nível do diretório acima de onde estou
 
-Database::getConnection();
+require_once(dirname(__FILE__, 2). '/src/models/User.php');
+
+$user = new User(['name' => 'rafael', '']);
+echo $user->getSelect('id');
