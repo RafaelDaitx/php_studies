c8679ac523a81f3c5cac506355b843087921e9f5
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2025-03-02 19:07:54 -0300

    Login create and database configuration

--- public/index.php
+++ public/index.php
@@ -3,4 +3,16 @@
 require_once(dirname(__FILE__, 2). '/src/config/config.php');
 //o 2 representa o nível do diretório acima de onde estou
 
-require_once(dirname(__FILE__, 2). '/src/views/login.php');
+require_once(MODEL_PATH .  '/Login.php');
+
+$login = new Login([
+    'email' => 'admin@cod3r.com.br',
+    'password' => 'a'
+]);
+
+try {
+    $login->checkLogin();
+    echo 'deu certo';
+} catch (Exception $e) {
+    echo "Problema no login :P";
+}
--- src/config/config.php
+++ src/config/config.php
@@ -4,5 +4,8 @@ setlocale(LC_TIME, 'pt-br', 'pt-br.utf-8', 'portuguese');
 
 //files
 define('MODEL_PATH', realpath(dirname(__FILE__). '/../models'));
+define('VIEW_PATH', realpath(dirname(__FILE__). '/../views'));
 
+//Files to imports
 require_once(realpath(dirname(__FILE__). '/database.php'));
+require_once(realpath(MODEL_PATH . '/Model.php'));
