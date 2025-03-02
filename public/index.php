d40cf6f9ffdf7496c4a0311ee4989e24fb442a43
Rafael Daitx <rafa.daitx.silva@rede.ulbra.br>
2025-03-02 16:35:15 -0300

    Login create and database configuration

--- public/index.php
+++ public/index.php
@@ -3,7 +3,4 @@
 require_once(dirname(__FILE__, 2). '/src/config/config.php');
 //o 2 representa o nível do diretório acima de onde estou
 
-require_once(dirname(__FILE__, 2). '/src/models/User.php');
-
-$user = new User(['name' => 'rafael', '']);
-echo $user->getSelect('id');
+require_once(dirname(__FILE__, 2). '/src/views/login.php');
